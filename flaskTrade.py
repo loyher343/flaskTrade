b2ac2307c3b6cdec13e900ea8b3f990387504120
Loy <loys343@pop-os.localdomain>
2021-01-22 04:30:40 -0600

    sends email messages

--- flaskTrade.py
+++ flaskTrade.py
@@ -1,6 +1,32 @@
 from flask import Flask
+from flask_mail import Mail, Message
+from config import *
 app = Flask(__name__)
 
-@app.route('/')
+app.config['DEBUG']= True
+app.config['TESTING']=False
+app.config['MAIL_SERVER']= MAIL_SERVER
+app.config['MAIL_PORT']=PORT
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
+#app.config['MAIL_DEBUG']=
+app.config['MAIL_USERNAME']=EMAIL
+app.config['MAIL_PASSWORD']=PASSWORD
+app.config['MAIL_DEFAULT_SENDER']= EMAIL
+app.config['MAIL_MAIL_MAX_EMAILS']=5
+#app.config['MAIL_MAIL_SUPPRESS_SEND']=False
+app.config['MAIL_ASCII_ATTACHMENTS']=False
+
+mail = Mail(app)
+
+
+
+@app.route('/mail')
 def hello_world():
-    return 'hello'
+    msg = Message('YOOOO', recipients=[RECIPIENT])
+    msg.html = '<b>This is the new test. yo</b>'
+    mail.send(msg)
+    return 'Message sent :)'
+
+if __name__ == '__main__':
+    app.run()
